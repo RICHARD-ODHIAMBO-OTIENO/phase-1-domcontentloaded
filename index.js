500cd8c229ed270db7da7baac6c35c6c5754dadd
Richard Odhiambo Otieno <kotienor1@gmail.com>
2024-04-10 15:57:16 +0300

    DOM Content Loaded

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+// create up a DOMContentLoaded event listener to detect when the HTML page has loaded 
+document.addEventListener('DOMContentLoaded', function() {
+    let textElement = document.getElementById('text');
+    textElement.textContent = 'This is really cool!';
+});
